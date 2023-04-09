8593ce0cbba35f342345c95ad1ed4a32087c959d
BasilRom <91020707+BasilRom@users.noreply.github.com>
2023-04-09 19:05:54 +0300

    Create main_pensil.py

---
+++ main_pensil.py
@@ -0,0 +1,21 @@
+import random
+import time
+
+list_of_cats = ['Мэри Шелли', 'Фламбуша', 'Петра', 'Родезия', 'Цукерка', 'Арракис', 'Комбат']
+
+def happy_cat():
+  happy_cat_today = random.choice(list_of_cats)
+  mes = f'Сегодня лучшее угощение получает {happy_cat_today}'
+
+def choice_action():
+   feed_cat = input('Хотите угостить кота? --> (да/нет')
+
+if feed_cat == 'да' or 'Да':
+  happy_cat()
+  
+elif feed_cat == 'нет' or 'Нет':
+  print('Ах вы жадина!!!')
+  choice_action()
+else:
+  print('До свидания')
+  time.sleep(1)
