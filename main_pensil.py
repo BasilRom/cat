d500f2cda247d51223b9e91b730d1b04b59354b3
BasilRom <91020707+BasilRom@users.noreply.github.com>
2023-04-10 22:25:40 +0300

    Update main_pensil.py

--- main_pensil.py
+++ main_pensil.py
@@ -1,42 +1,25 @@
 import random
 import time
 
-list_of_cats = ['Мэри Шелли', 'Фламбуша', 'Петра', 'Родезия', 'Цукерка', 'Арракис', 'Комбат']
-
-def happy_cat():
-  happy_cat_today = random.choice(list_of_cats)
-  mes = f'Сегодня лучшее угощение получает {happy_cat_today}'
-import random
-import time
 
 list_of_cats = ['Мэри Шелли', 'Фламбуша', 'Петра', 'Родезия', 'Цукерка', 'Арракис', 'Комбат']
 
 def happy_cat():
   happy_cat_today = random.choice(list_of_cats)
   mes = f'Сегодня лучшее угощение получает {happy_cat_today}'
+  print(mes)
 
 def choice_action():
    feed_cat = input('Хотите угостить кота? --> (да/нет')
 
-if feed_cat == 'да' or 'Да':
-  happy_cat()
+   if feed_cat == 'да' or if feed_cat == 'Да':
+     happy_cat()
 
-elif feed_cat == 'нет' or 'Нет':
-  print('Ах вы жадина!!!')
-  choice_action()
-else:
-  print('До свидания')
-  time.sleep(1)
+   elif feed_cat == 'нет' or if feed_cat =='Нет':
+     print('Ах вы жадина!!!')
+     choice_action()
+   else:
+     print('До свидания')
+     time.sleep(1)
 
-def choice_action():
-   feed_cat = input('Хотите угостить кота? --> (да/нет')
-
-if feed_cat == 'да' or 'Да':
-  happy_cat()
-  
-elif feed_cat == 'нет' or 'Нет':
-  print('Ах вы жадина!!!')
-  choice_action()
-else:
-  print('До свидания')
-  time.sleep(1)
+choise_action()
